b2b99e1a274aed9c1c036a158ba03e6359fa3c4b
VARNANAKV <135126547+VARNANAKV@users.noreply.github.com>
2023-06-19 10:20:18 +0530

    Add files via upload

---
+++ motion.py
@@ -0,0 +1,35 @@
+import cv2
+from playsound import playsound
+
+# Initialize the camera
+cam = cv2.VideoCapture(0)
+
+# Path to the sound file
+sound_file = '/home/varnana/Visual_studio/face_recognition/BuzzerWav.wav'
+
+while cam.isOpened():
+    ret, frame1 = cam.read()
+    ret, frame2 = cam.read()
+    diff = cv2.absdiff(frame1, frame2)
+    grey_video = cv2.cvtColor(diff, cv2.COLOR_RGB2GRAY)
+    blur = cv2.GaussianBlur(grey_video, (5, 5), 0)
+    _, thresh = cv2.threshold(blur, 20, 255, cv2.THRESH_BINARY)
+    dilated = cv2.dilate(thresh, None, iterations=3)
+    contours, _ = cv2.findContours(dilated, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+    for c in contours:
+        if cv2.contourArea(c) < 5000:
+            continue
+        x, y, w, h = cv2.boundingRect(c)
+        cv2.rectangle(frame1, (x, y), (x+w, y+h), (0, 255, 0), thickness=2)
+
+        # Play a sound when a face is detected
+        playsound(sound_file)
+
+    if cv2.waitKey(10) & 0xFF == 27:
+        break
+
+    cv2.imshow('Security cam', frame1)
+
+cam.release()
+cv2.destroyAllWindows()
